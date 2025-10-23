f53ad8756e16866a1b8a3b5c372b66417ed63b33
junUTE <vuquoctrung0@gmail.com>
2025-10-23 15:15:25 +0700

    update

--- UTESHOP/src/main/java/ute/utils/Constant.java
+++ UTESHOP/src/main/java/ute/utils/Constant.java
@@ -14,5 +14,14 @@ public class Constant {
 	public static final String STATUS_ACTIVE = "ACTIVE";
 	public static final String STATUS_INACTIVE = "INACTIVE";
 	public static final String STATUS_PENDING = "PENDING";
-
+	
+	// Order Status
+	public static final String ORDER_PENDING = "PENDING";
+	public static final String ORDER_SHIPPED = "SHIPPED";
+	public static final String ORDER_DELIVERED = "DELIVERED";
+	public static final String ORDER_CANCELED = "CANCELED";
+	
+	// payment status
+	public static final String PAYMENT_PAID = "PAID";
+	public static final String PAYMENT_UNPAID = "UNPAID";
 }
