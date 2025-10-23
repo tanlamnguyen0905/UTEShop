2d4217d7365c58d84d851e9622b345984a660328
Nguyen Lam Tan <tanbeo0905@gmail.com>
2025-10-23 10:01:58 +0700

    detail product

--- UTESHOP/src/main/java/ute/entities/Product.java
+++ UTESHOP/src/main/java/ute/entities/Product.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -59,7 +60,7 @@ public class Product {
     @JoinColumn(name = "brandID")
     private Brand brand;
 
-    @OneToMany(mappedBy = "product")
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "product")
     private List<Review> reviews;
 
     @OneToMany(mappedBy = "product")
